3415fd492beced6b787f6585e0524eddcabff8e7
Christopher Phan <phanc2069@my.sduhsd.net>
2020-01-17 13:32:23 -0800

    FINISH

---
+++ fproj.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+int main (int argc, char *argv[]) {
+ int rows, coef=1, space, i, j;
+ if(atoi(argv[1])>=atoi(argv[2])) {
+   if(isdigit(*argv[1])) {
+     rows = atoi(argv[1]);
+     for (i=0; i<rows; i++) {
+       for (space=1; space <= rows-i; space++)
+           printf("  ");
+       for (j=0; j<=i; j++) {
+         if (j==0 || i==0) {
+             coef = 1;
+	 }
+         else
+             coef=coef*(i-j+1)/j;
+          printf("%4d", coef);
+       }
+       printf("\n");
+    }
+  }
+    if(argv[2] == 0) {
+    for(i=(rows-1); i<rows; i++) {
+     for (space=1; space <= rows-i; space++) {
+           printf("  ");
+      }     
+      for (j=0; j<=i; j++) {
+       if (j==0 || i==0) {
+          coef = 1;
+       }   
+         else {
+             coef=coef*(i-j+1)/j;
+             printf("%4d", coef);
+         }    
+     }
+     printf("\n");
+  }
+  return 0;
+ }
+  if(isdigit(*argv[2])) {
+  if(atoi(argv[2]) != 0) {
+   for(i=(rows-1); i<rows; i++) {
+        for (j=(atoi(argv[2]+3)); j<atoi(argv[2]); j++) {
+          if (j==0 || i==0) {
+              coef = 1;
+          }   
+          else {
+              coef=coef*(i-j+1)/j;
+          }
+        }
+      coef = abs(coef);  
+      printf("\nIn row %d, the value you seek is %d.", rows, coef);
+      printf("\n");
+     }
+    }
+    else {
+      for(i=(rows-1); i<rows; i++) {
+      for (space=1; space <= rows-i; space++)
+            printf("  ");
+        for (j=0; j<=i; j++) {
+          if (j==0 || i==0)
+              coef = 1;
+          else
+              coef=coef*(i-j+1)/j;
+              printf("%4d", coef);
+      }
+      printf("\nHere is row %d", rows);
+      printf("\n");
+    }
+   }
+  }
+ }
+}
